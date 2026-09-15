6368618391c505ffece7efbaa209820d29426bfa
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-15 17:35:20 +0700

    Start advanced feature coordinator with app lifecycle

--- lib/src/widgets/screen_awake_observer.dart
+++ lib/src/widgets/screen_awake_observer.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../providers/audio_provider.dart';
 import '../providers/settings_provider.dart';
+import '../services/kikoflu_feature_coordinator.dart';
 import '../services/screen_awake_service.dart';
 import 'app_lock_gate.dart';
 
@@ -23,8 +26,17 @@ class _ScreenAwakeObserverState extends ConsumerState<ScreenAwakeObserver> {
   bool? _lastRequestedEnabled;
 
   @override
+  void initState() {
+    super.initState();
+    // The coordinator only observes existing Hiraukan services. All imported
+    // features remain opt-in and therefore add no source/provider startup work.
+    unawaited(KikoFluFeatureCoordinator.instance.initialize());
+  }
+
+  @override
   void dispose() {
     ScreenAwakeService.setEnabled(false);
+    unawaited(KikoFluFeatureCoordinator.instance.dispose());
     super.dispose();
   }
 
