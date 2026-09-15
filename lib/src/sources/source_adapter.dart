2da57884cad7a1ca5ef02a5fb4ad0407b797b6e5
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-15 07:50:54 +0700

    Add unified source adapter contract

---
+++ lib/src/sources/source_adapter.dart
@@ -0,0 +1,18 @@
+import '../models/work.dart';
+import 'unified_source_models.dart';
+
+abstract class UnifiedSourceAdapter {
+  UnifiedSourceKind get kind;
+
+  Future<SourceSearchPage> search({
+    required String keyword,
+    required int page,
+    required int pageSize,
+  });
+
+  Future<Work> loadDetail(UnifiedSourceRef ref);
+
+  Future<List<dynamic>> loadTracks(UnifiedSourceRef ref);
+
+  Future<UnifiedSourceHealth> checkHealth();
+}
