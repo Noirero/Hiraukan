fc2edb6c53540ec8babf4bc06c0a28df6c07add6
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-16 08:42:56 +0700

    Fix unified source capability test package import

--- test/unified_source_capability_test.dart
+++ test/unified_source_capability_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hiraukan/src/sources/unified_source_models.dart';
+import 'package:kikoeru_flutter/src/sources/unified_source_models.dart';
 
 void main() {
   group('unified source capabilities', () {
