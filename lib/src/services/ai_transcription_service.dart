978ef3f0ac78b31d5323530ca645ffa1c94244c2
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-19 02:09:25 +0700

    fix: remove unused transcription import

--- lib/src/services/ai_transcription_service.dart
+++ lib/src/services/ai_transcription_service.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as p;
 import 'package:wakelock_plus/wakelock_plus.dart';
 import 'package:whisper_ggml_plus/whisper_ggml_plus.dart';
