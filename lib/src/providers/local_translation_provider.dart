2faab9852fb43b600c6f0e04cf4bd7c7d0733a55
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-20 02:04:13 +0700

    beta translation: expose lightweight engine contract

--- lib/src/providers/local_translation_provider.dart
+++ lib/src/providers/local_translation_provider.dart
@@ -1,11 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../services/free_online_translation_engine.dart';
-import '../services/local_translation_engine.dart';
+import '../services/translation_engine.dart';
 import '../services/subtitle_translation_cache.dart';
 
 final freeOnlineTranslationEngineProvider =
-    Provider<LocalTranslationEngine>((ref) {
+    Provider<TranslationEngine>((ref) {
   return FreeOnlineTranslationEngine.instance;
 });
 
