699fe524cd7433e5018b07b054dc6c2cc84d42f1
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-20 03:52:11 +0700

    beta translation: add canonical translation provider

---
+++ lib/src/providers/translation_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../services/free_online_translation_engine.dart';
+import '../services/subtitle_translation_cache.dart';
+import '../services/translation_engine.dart';
+
+final freeOnlineTranslationEngineProvider =
+    Provider<TranslationEngine>((ref) {
+  return FreeOnlineTranslationEngine.instance;
+});
+
+final translationDocumentCacheStatsProvider =
+    FutureProvider<TranslationDocumentCacheStats>((ref) {
+  return SubtitleTranslationCache.instance.stats();
+});
