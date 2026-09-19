29ca478d4d1ce3d097cbb6924d320fc5ac8cafda
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-20 02:01:35 +0700

    beta translation: remove obsolete model manager state

--- lib/src/providers/local_translation_provider.dart
+++ lib/src/providers/local_translation_provider.dart
@@ -1,56 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../services/local_translation_engine.dart';
 import '../services/free_online_translation_engine.dart';
+import '../services/local_translation_engine.dart';
 import '../services/subtitle_translation_cache.dart';
 
-class LocalTranslationModelNotifier
-    extends StateNotifier<AsyncValue<LocalTranslationModelStatus>> {
-  LocalTranslationModelNotifier(this._engine)
-      : super(const AsyncValue.loading()) {
-    refresh();
-  }
-
-  final LocalTranslationEngine _engine;
-
-  Future<void> refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(_engine.getModelStatus);
-  }
-
-  Future<void> download({bool wifiOnly = true}) async {
-    state = AsyncValue.data(
-      LocalTranslationModelStatus(
-        state: LocalModelState.downloading,
-        engineId: _engine.id,
-        engineVersion: _engine.version,
-        sourceModelInstalled: false,
-        targetModelInstalled: false,
-      ),
-    );
-    state = await AsyncValue.guard(
-      () => _engine.downloadModels(wifiOnly: wifiOnly),
-    );
-  }
-
-  Future<void> delete() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(_engine.deleteModels);
-  }
-}
-
-final localTranslationEngineProvider = Provider<LocalTranslationEngine>((ref) {
+final freeOnlineTranslationEngineProvider =
+    Provider<LocalTranslationEngine>((ref) {
   return FreeOnlineTranslationEngine.instance;
 });
-
-final localTranslationModelProvider = StateNotifierProvider<
-    LocalTranslationModelNotifier,
-    AsyncValue<LocalTranslationModelStatus>>((ref) {
-  return LocalTranslationModelNotifier(
-    ref.watch(localTranslationEngineProvider),
-  );
-});
-
 
 final translationDocumentCacheStatsProvider =
     FutureProvider<TranslationDocumentCacheStats>((ref) {
