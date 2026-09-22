8d97238bbd6de3f27658f682cf3fc06b4a0b945f
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-23 01:03:29 +0700

    P3: gate bundled audio runtimes by install state

--- lib/src/extensions/audio_extension_provider.dart
+++ lib/src/extensions/audio_extension_provider.dart
@@ -3,17 +3,34 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../providers/auth_provider.dart';
 import '../services/kikoeru_api_service.dart';
 import 'asmr_one_audio_extension.dart';
+import 'audio_extension.dart';
+import 'audio_extension_install_provider.dart';
 import 'audio_extension_registry.dart';
 
-final audioExtensionRegistryProvider = Provider<AudioExtensionRegistry>((ref) {
+final bundledAudioExtensionsProvider = Provider<List<AudioExtension>>((ref) {
   final api = ref.watch(kikoeruApiServiceProvider);
   final auth = ref.watch(authProvider);
 
-  return AudioExtensionRegistry([
+  return [
     AsmrOneAudioExtension(
       api: api,
       host: () => auth.host ?? KikoeruApiService.remoteHost,
       token: () => auth.token ?? '',
     ),
-  ]);
+  ];
+});
+
+final audioExtensionInstallProvider = StateNotifierProvider<
+    AudioExtensionInstallController, AudioExtensionInstallState>((ref) {
+  return AudioExtensionInstallController(
+    ref.watch(bundledAudioExtensionsProvider),
+  );
+});
+
+final audioExtensionRegistryProvider = Provider<AudioExtensionRegistry>((ref) {
+  final bundled = ref.watch(bundledAudioExtensionsProvider);
+  final installed = ref.watch(audioExtensionInstallProvider).installedIds;
+  return AudioExtensionRegistry(
+    bundled.where((extension) => installed.contains(extension.manifest.id)),
+  );
 });
