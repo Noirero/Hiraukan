03f5d90f48460955c75d98032656d66190279b17
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-23 01:06:06 +0700

    P5: reject duplicate audio extension runtimes

--- lib/src/extensions/audio_extension_registry.dart
+++ lib/src/extensions/audio_extension_registry.dart
@@ -4,9 +4,21 @@ class AudioExtensionRegistry {
   final Map<String, AudioExtension> _extensions;
 
   AudioExtensionRegistry(Iterable<AudioExtension> extensions)
-      : _extensions = {
-          for (final extension in extensions) extension.manifest.id: extension,
-        };
+      : _extensions = _buildRegistry(extensions);
+
+  static Map<String, AudioExtension> _buildRegistry(
+    Iterable<AudioExtension> extensions,
+  ) {
+    final result = <String, AudioExtension>{};
+    for (final extension in extensions) {
+      final id = extension.manifest.id;
+      if (result.containsKey(id)) {
+        throw StateError('Duplicate audio extension runtime id: ' + id);
+      }
+      result[id] = extension;
+    }
+    return result;
+  }
 
   List<AudioExtension> get extensions =>
       _extensions.values.toList(growable: false);
