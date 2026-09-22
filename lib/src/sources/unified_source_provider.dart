262228a12755d973682a5c5749f05baf6222b9b7
LatishaLIFE <149634319+Noirero@users.noreply.github.com>
2026-09-23 01:08:29 +0700

    P4: disable unified source adapters when extension is removed

--- lib/src/sources/unified_source_provider.dart
+++ lib/src/sources/unified_source_provider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../extensions/audio_extension_install_provider.dart';
+import '../extensions/audio_extension_provider.dart';
 import '../providers/auth_provider.dart';
 import 'asmr_one_source_adapter.dart';
 import 'ero_voice_source_adapter.dart';
@@ -9,11 +11,15 @@ import 'unified_source_service.dart';
 
 final unifiedSourceServiceProvider = Provider<UnifiedSourceService>((ref) {
   final kikoeru = ref.watch(kikoeruApiServiceProvider);
+  final installed = ref.watch(audioExtensionInstallProvider).installedIds;
+
+  bool enabled(String id) => installed.contains(id);
+
   return UnifiedSourceService(
     adapters: [
-      AsmrOneSourceAdapter(kikoeru),
-      HentaiAsmrSourceAdapter(),
-      EroVoiceSourceAdapter(),
+      if (enabled('miyorare.audio.asmr_one')) AsmrOneSourceAdapter(kikoeru),
+      if (enabled('miyorare.audio.hentai_asmr')) HentaiAsmrSourceAdapter(),
+      if (enabled('miyorare.audio.ero_voice')) EroVoiceSourceAdapter(),
     ],
     registry: UnifiedSourceRegistry.instance,
   );
